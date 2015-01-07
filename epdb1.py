03fe4a35946b5b91d552f17bbd798e9a9d581661
Jeff Wright <jeff.washcloth@gmail.com>
2015-01-07 15:42:14 -0500

    Initial experiment using "set_trace()" method

--- epdb1.py
+++ epdb1.py
@@ -1,6 +1,13 @@
 # epdb1.py -- experiment with the Python debugger, pdb
+
+import pdb
+
 a = "aaa"
+pdb.set_trace()
 b = "bbb"
+pdb.set_trace()
 c = "ccc"
+pdb.set_trace()
 final = a + b + c
+pdb.set_trace()
 print final
